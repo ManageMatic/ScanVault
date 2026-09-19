6a2c590c7b2fcc81d8666080eaf45ade7aa40f79
ManageMatic <ishanmahida123@gmail.com>
2026-09-19 13:04:59 +0530

    fix: increase mobile viewport bottom padding in AppShell to prevent occlusion by bottom navigation

--- apps/web/src/layouts/AppShell.tsx
+++ apps/web/src/layouts/AppShell.tsx
@@ -18,8 +18,8 @@ export function AppShell({ children }: AppShellProps) {
         {/* Mobile Header */}
         <MobileHeader />
 
-        {/* Scrollable Page Viewport */}
-        <main className="flex-1 w-full max-w-5xl mx-auto px-4 xs:px-5 py-5 xs:py-6 pb-28 lg:pb-8 flex flex-col">
+        {/* Scrollable Page Viewport with generous bottom padding on mobile for fixed navbar and floating scan trigger */}
+        <main className="flex-1 w-full max-w-5xl mx-auto px-4 xs:px-5 py-5 xs:py-6 pb-36 lg:pb-10 flex flex-col">
           {children}
         </main>
 
